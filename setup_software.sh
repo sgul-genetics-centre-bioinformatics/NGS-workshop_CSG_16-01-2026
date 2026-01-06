24b230b025e32557dcfa6e78883ec814dfbb1009
saradobbins <82818102+saradobbins@users.noreply.github.com>
2026-01-06 12:08:09 +0000

    Update setup_software.sh

--- setup_software.sh
+++ setup_software.sh
@@ -4,5 +4,8 @@ ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics
 ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/samtools/samtools samtools
 ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/fastp/0.20/fastp fastp
 ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/Genome_reference_files/common_all_chrM.vcf.gz common_all_chrM.vcf.gz
-ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/Genome_reference_files/common_all_chrM.vcf.gz.tbi Genome_reference_files/common_all_chrM.vcf.gz.tbi
-
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/Genome_reference_files/common_all_chrM.vcf.gz.tbi common_all_chrM.vcf.gz.tbi
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/FastQC/fastqc fastqc
+ls -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/gatk-4.0.4.0/gatk-package-4.0.4.0-local.jar gatk-package-4.0.4.0-local.jar
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/picard-2.815/picard.jar picard.jar
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/samtools-1.8/samtools samtools
