94a65bd4fa94e94e0784339cbf9f9595fa1c51ab
saradobbins <82818102+saradobbins@users.noreply.github.com>
2026-01-06 11:59:22 +0000

    Update setup_software.sh

--- setup_software.sh
+++ setup_software.sh
@@ -1,7 +1,3 @@
-## Software Setup
-Run these commands after cloning:
-
-```bash
 mkdir software_update
 cd software_update
 ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/bwa/bwa bwa
@@ -9,4 +5,4 @@ ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics
 ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/fastp/0.20/fastp fastp
 ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/Genome_reference_files/common_all_chrM.vcf.gz common_all_chrM.vcf.gz
 ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/Genome_reference_files/common_all_chrM.vcf.gz.tbi Genome_reference_files/common_all_chrM.vcf.gz.tbi
-```
+
