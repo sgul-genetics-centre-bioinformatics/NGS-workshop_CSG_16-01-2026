144cf322769b0db166a3591ca93177bc90c3f6f7
saradobbins <82818102+saradobbins@users.noreply.github.com>
2026-01-06 11:58:24 +0000

    Create setup_software.sh
    Script to create soft links to software required for project

---
+++ setup_software.sh
@@ -0,0 +1,12 @@
+## Software Setup
+Run these commands after cloning:
+
+```bash
+mkdir software_update
+cd software_update
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/bwa/bwa bwa
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/samtools/samtools samtools
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/fastp/0.20/fastp fastp
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/Genome_reference_files/common_all_chrM.vcf.gz common_all_chrM.vcf.gz
+ln -s /homes/athosnew/Genetics_Centre_Bioinformatics/NGS_workshop_bioinformatics_unit_31_01_2020_software/Genome_reference_files/common_all_chrM.vcf.gz.tbi Genome_reference_files/common_all_chrM.vcf.gz.tbi
+```
